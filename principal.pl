925890999f5a18e024a1bc8c14bae5bb12452491
Yagho Mattos <mattosyagho@gmail.com>
2022-11-11 23:57:40 -0300

    load file, save operacao, remove '()', frame lucro

--- principal.pl
+++ principal.pl
@@ -16,6 +16,10 @@ salva(Predicado,Arquivo) :-
  listing(Predicado),
  told.
 
+carrega(A) :-
+ exists_file(A),
+ consult(A).
+
 %-----------------------------------------------------------------------------------------
 
 
@@ -31,7 +35,7 @@ empresa("VIIA3", 4.03).
 
 cadastro(X,Y) :- assertz(empresa(X,Y)).
 
-empresas() :- listing(empresa/2).
+empresas :- listing(empresa/2).
 %-----------------------------------------------------------------------------------------
 
 
@@ -40,10 +44,13 @@ empresas() :- listing(empresa/2).
 
 :- dynamic operacao/7.
 
-cadastro_operacao(X, Y, Z, P, N) :- T is N * P * 0.00025 + N * P * 0.00005, C is P * N + T, assertz(operacao(X, Y, Z, P, N, T, C)). 
+cadastro_operacao(D, A, O, P, N) :- 
+  T is N * P * 0.00025 + N * P * 0.00005,
+  C is P * N + T, 
+  assertz(operacao(D, A, O, P, N, T, C)),
+  salva(operacao(D, A, O, P, N, T, C), 'operacoes.bd'). 
 
-%operacao(Data, Ticket, "Compra", Preço Unitário, Quantidade, Taxas, CustoTotal)
-operacao(11/03/2020, "PETR4", "Compra", 15.34, 200, 0.92, 3068.92).
+/*operacao(11/03/2020, "PETR4", "Compra", 15.34, 200, 0.92, 3068.92).
 operacao(12/03/2020, "PETR4", "Compra", 12.68, 300, 1.14, 3805.14).
 operacao(18/03/2020, "PETR4", "Compra", 10.98, 100, 0.32, 1098.32).
 operacao(20/03/2020, "ITSA4", "Compra", 8.51, 100, 0.25, 851.25).
@@ -57,9 +64,6 @@ operacao(03/04/2020, "BBSE3", "Compra", 22.81, 100, 0.68, 2281.68).
 operacao(03/04/2020, "CMIG4", "Compra", 7.99, 100, 0.23, 799.23).
 operacao(24/04/2020, "BBAS3", "Compra", 24.99, 200, 1.49, 4999.49).
 operacao(07/05/2020, "CIEL3", "Compra", 3.62, 200, 0.21, 362.21).
-
-
-%venda(Data, Ticket, "Venda", Preço Unitário, Quantidade, Taxas, Custo Total)
 operacao(19/03/2020, "PETR4", "Venda", 12.60, -100, 0.37, -1259.63).
 operacao(02/04/2020, "PETR4", "Venda", 16.00, -200, 0.96, -3199.04).
 operacao(07/04/2020, "ITSA4", "Venda", 9.10, -100, 0.27, -909.73).
@@ -69,9 +73,11 @@ operacao(08/04/2020, "MOVI3", "Venda", 9.18, -200, 0.55, -1835.45).
 operacao(09/04/2020, "VIIA3", "Venda", 5.41, -100, 0.16, -540.84).
 operacao(09/04/2020, "CMIG4", "Venda", 9.42, -100, 0.28, -941.72).
 operacao(14/04/2020, "BBSE3", "Venda", 27.00, -100, 0.81, -2699.19).
-operacao(15/04/2020, "AMAR3", "Venda", 5.54, -300, 0.49, -1661.51).
+operacao(15/04/2020, "AMAR3", "Venda", 5.54, -300, 0.49, -1661.51).*/
 
-operacoes() :- salva(operacao(_,_,_,_,_,_,_),'operacoes.pl'), listing(operacao/7).
+operacoes :- 
+  carrega('operacoes.bd'),
+  listing(operacao/7).
 %-----------------------------------------------------------------------------------------
 
 
@@ -80,14 +86,32 @@ operacoes() :- salva(operacao(_,_,_,_,_,_,_),'operacoes.pl'), listing(operacao/7
 
 :- dynamic acao/4.
 
-calcula() :- retractall(acao(A,B,C,D)), forall(operacao(_,A,_,B,C,_,D), assert(acao(A,B,C,D))), salva(acao(_,_,_,_),'carteira.pl').
+calcula_carteira :- 
+  retractall(acao(A,B,C,D)), 
+  forall(operacao(_,A,_,B,C,_,D), assert(acao(A,B,C,D))). 
+%  salva(acao(_,_,_,_),'carteira.bd').
 
-carteira() :- calcula(), listing(acao/4).
+carteira :- 
+  calcula_carteira, 
+  listing(acao/4).
 %-----------------------------------------------------------------------------------------
 
 
 %-----------------------------------------------------------------------------------------
 %lucro / prejuízo
+
+:- dynamic ano/2.
+:- dynamic soma/1.
+:- dynamic atual/1.
+
+calcula_lucro :- 
+  forall(operacao(_,A,_,_,_,_,C), A =:= atual(_) -> soma(soma(_) + C); atual(A), soma(soma(_) + C), assert(ano(A,C))).
+
+lucro :- 
+  A is operacao(_,A,_,_,_,_,_),
+  atual(A),
+  calcula_lucro,
+  listing(ano/2).
 %-----------------------------------------------------------------------------------------
 
 
